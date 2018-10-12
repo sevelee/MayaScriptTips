4a369ea6817d1c2c59036320b688a0c9a985ce38
sevelee <seve.lee@hotmail.com>
2018-10-12 14:22:05 +0800

    update

--- Common.py
+++ Common.py
@@ -16,3 +16,17 @@ else:
     from PySide2 import QtCore
     from PySide2 import QtWidgets
     from PySide2 import QtUiTools
+
+# 更科学一点的方式
+try:
+    from PySide2 import QtGui
+    from PySide2 import QtCore
+    from PySide2 import QtWidgets
+    from PySide2 import QtUiTools
+    from shiboken2 import wrapInstance
+except ImportError:
+    from PySide import QtGui
+    from PySide import QtCore
+    from PySide import QtWidgets
+    from PySide import QtUiTools
+    from shiboken import wrapInstance
